d05d3964d6848cd18437b75f611cec6420f87d61
Sam <samuelanderson@me.com>
2024-05-26 20:12:32 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+
+// FAQ accordion
+
 document.addEventListener('DOMContentLoaded', function() {
     var faqQuestions = document.querySelectorAll('.faq-question');
 
@@ -14,28 +18,39 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+// Bike animation
+
+
+var isAnimating = false;
+var bike = document.querySelector('.bike'); 
+
 window.onscroll = function() {
     var scrollBarHeight = window.innerHeight - document.documentElement.clientHeight;
-    var bike = document.querySelector('.bike');
-    var triggerPoint = document.body.offsetHeight - 100; // Adjust this value to change when the animation is triggered
   
-    if ((window.innerHeight + window.scrollY - scrollBarHeight) >= document.body.offsetHeight) {
-      bike.classList.remove('animate');
-      void bike.offsetHeight; // Trigger reflow to restart animation
+    if (!isAnimating && (window.innerHeight + window.scrollY - scrollBarHeight) >= document.body.offsetHeight) {
+        isAnimating = true;
+        bike.classList.remove('animate');
+        void bike.offsetHeight; // Trigger reflow to restart animation
   
-      setTimeout(function() {
-        bike.classList.add('animate');
-      }, 10); // Short delay to allow the browser to recognize the class removal before adding it back
+        setTimeout(function() {
+            bike.classList.add('animate');
+        }, 10); // Short delay to allow the browser to recognize the class removal before adding it back
     }
-  };
+};
 
-  var bike = document.querySelector('.bike');
-
-bike.addEventListener('click', function() {
-  bike.classList.remove('animate');
-  void bike.offsetHeight;
+bike.addEventListener('animationend', function() {
+    isAnimating = false; 
+});
   
-  setTimeout(function() {
-    bike.classList.add('animate');
-  }, 10); // Short delay to allow the browser to recognize the class removal before adding it back
+bike.addEventListener('click', function() {
+    if (!isAnimating) { 
+        isAnimating = true; 
+        bike.classList.remove('animate');
+        void bike.offsetHeight;
+    
+        setTimeout(function() {
+            bike.classList.add('animate');
+        }, 10); // Short delay to allow the browser to recognize the class removal before adding it back
+    }
 });
